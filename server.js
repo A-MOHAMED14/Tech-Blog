1fb599af507ee4bcba233b8a284a2613deb9748a
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-24 15:44:35 +0100

    Set template and view engine to handlebars

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const routes = require("./controllers/index.js");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

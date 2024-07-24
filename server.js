785ec8c1a9a21e04adb099cbc7ad3958b6e0d189
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-24 17:35:00 +0100

    serve static files from the public directory

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const sequelize = require("./config/connection.js");
 const exphbs = require("express-handlebars");
@@ -14,6 +15,8 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 

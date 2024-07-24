b277d98c7b7567536a351a64a04a225ab515be37
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-24 15:43:50 +0100

    Rquire and use routes from controllers

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const sequelize = require("./config/connection.js");
+const exphbs = require("express-handlebars");
+
+const routes = require("./controllers/index.js");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(
-    (PORT, () => console.log(`Server is now running on Port ${PORT}...`))
-  );
+  app.listen(PORT, () => console.log(`Server is running on Port ${PORT}...`));
 });

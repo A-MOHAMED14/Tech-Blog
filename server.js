57a1cb282c40bfb60e245ace629130642affbc78
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-25 12:50:16 +0100

    Import express-session and set up sessions

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection.js");
 const routes = require("./controllers/index.js");
-const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers.js");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 600000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 

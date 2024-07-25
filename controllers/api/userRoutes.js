d3a2ebc9fa8283743ccbedcefd654f33a71ec9e4
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-25 12:38:04 +0100

    Add POST route handler for user login with error handling

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -10,3 +10,29 @@ router.post("/", async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    res.json({ user: userData, message: "You are now logged in!" });
+  } catch (error) {
+    res.status(400).json(err);
+  }
+});

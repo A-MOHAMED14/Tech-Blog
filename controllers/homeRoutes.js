a336b43d4f7117a51a597ff47ada8b0bf92149e5
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-25 13:37:25 +0100

    Redirect user to login form when they click login button

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,4 +20,13 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+
 module.exports = router;
